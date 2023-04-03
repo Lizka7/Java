3bf610d39467b0f0a54276374692192f0c22ab13
Lizka7 <128884829+Lizka7@users.noreply.github.com>
2023-04-03 14:03:11 +0300

    Add files via upload

--- symbols/src/main/java/com/example/symbols/CountController.java
+++ symbols/src/main/java/com/example/symbols/CountController.java
@@ -2,9 +2,7 @@ package com.example.symbols;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
@@ -13,10 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 
 @RestController
 public class CountController {
     private static final Logger logger = LoggerFactory.getLogger(CountController.class);
+
+    private final CountCache cache;
+
+    @Autowired
+    public CountController(CountCache cache) {
+        this.cache = cache;
+    }
 
     @GetMapping("/count")
     public ResponseEntity<CountResult> count(@RequestParam("string") String string, @RequestParam("symbol") String symbol) {
@@ -29,6 +35,15 @@ public class CountController {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Input string is empty");
             }
 
+            // Check if result is in cache
+            String cacheKey = string + symbol;
+            Integer cachedCount = cache.get(cacheKey);
+            if (cachedCount != null) {
+                CountResult cachedResult = new CountResult(string, symbol, cachedCount);
+                logger.info("Result retrieved from cache: string={}, symbol={}, result={}", string, symbol, cachedResult);
+                return ResponseEntity.ok(cachedResult);
+            }
+
             for (int i = 0; i < string.length(); i++) {
                 if (string.charAt(i) == s) {
                     count++;
@@ -37,6 +52,9 @@ public class CountController {
 
             CountResult result = new CountResult(string, symbol, count);
             logger.info("Count request received: string={}, symbol={}, result={}", string, symbol, result);
+
+            // Add result to cache
+            cache.put(cacheKey, count);
 
             return ResponseEntity.ok(result);
         } catch (MethodArgumentTypeMismatchException e) {
@@ -50,4 +68,10 @@ public class CountController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
+
+    @GetMapping("/cache")
+    public Map<String, Integer> showCache() {
+        return cache.getAll();
+    }
+
 }
--- symbols/src/main/java/com/example/symbols/CountResult.java
+++ symbols/src/main/java/com/example/symbols/CountResult.java
@@ -1,38 +1,37 @@
 package com.example.symbols;
 
 public class CountResult {
-        private String string;
-        private String symbol;
-        private int count;
+    private String string;
+    private String symbol;
+    private int count;
 
-        public CountResult(String string, String symbol, int count) {
-            this.string = string;
-            this.symbol = symbol;
-            this.count = count;
-        }
-
-        public String getString() {
-            return string;
-        }
-
-        public void setString(String string) {
-            this.string = string;
-        }
-
-        public String getSymbol() {
-            return symbol;
-        }
-
-        public void setSymbol(String symbol) {
-            this.symbol = symbol;
-        }
-
-        public int getCount() {
-            return count;
-        }
-
-        public void setCount(int count) {
-            this.count = count;
-        }
+    public CountResult(String string, String symbol, int count) {
+        this.string = string;
+        this.symbol = symbol;
+        this.count = count;
     }
 
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
